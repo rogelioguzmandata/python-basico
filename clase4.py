64b8a124f80c2fe9e11af241565fb1202365a876
ROGELIO A GUZMAN ROSAS <roge5000@gmail.com>
2025-11-19 23:05:28 +0100

    contenido de la clase 4

---
+++ clase4.py
@@ -0,0 +1,9 @@
+name= 'Rogelio Alberto'
+last_name= 'Guzman   Rosas'
+print( 5* name)
+print(name + ' ' + last_name)
+print(len(name))
+print(len(last_name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
