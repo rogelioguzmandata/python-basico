81c036a365446c532ebd8b97a1fd0d99777f8de7
ROGELIO A GUZMAN ROSAS <roge5000@gmail.com>
2025-11-20 21:12:05 +0100

    Delete ej 3.py

--- ej 3.py
+++
@@ -1,8 +0,0 @@
-precio= 1250000
-iva=25
-descuento= 12
-iva_numero= precio *(iva/100)
-total_con_iva= precio+iva_numero
-descuento_numero=total_con_iva*(12/100)
-total_con_descuento=total_con_iva-descuento_numero
-print(total_con_descuento)
