2cf6e498df5fe9f90f34a94d314f70efc8f0dadf
ROGELIO A GUZMAN ROSAS <roge5000@gmail.com>
2025-11-20 21:13:18 +0100

    Delete clase4.py

--- clase4.py
+++
@@ -1,9 +0,0 @@
-name= 'Rogelio Alberto'
-last_name= 'Guzman   Rosas'
-print( 5* name)
-print(name + ' ' + last_name)
-print(len(name))
-print(len(last_name))
-print(name.lower())
-print(name.upper())
-print(name.strip())
